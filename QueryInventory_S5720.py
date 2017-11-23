1215df58c09d38a11217e1919f9d46f337e1f9a2
Vitaly Nesterov <33909972+nestvit@users.noreply.github.com>
2017-11-23 14:35:51 +0300

    commit

--- QueryInventory_S5720.py
+++ QueryInventory_S5720.py
@@ -5,6 +5,10 @@ from datetime import datetime
 import paramiko
 huawei_5720 = {}
 
+import logging
+logging.basicConfig(filename='test.log', level=logging.DEBUG)
+logger = logging.getLogger("netmiko")
+
 try:
     pass
     name_module =6
@@ -14,12 +18,11 @@ finally:
     print("Модуль " + str(name_module) + " - OK")
 
 
-huawei_5720 = {'device_type': 'huawei_ssh','ip': '172.26.30.78','username': 'iteco','password': 'iteco2010'}
+huawei_5720 = {'device_type': 'huawei_ssh','ip': '172.26.30.78','username': 'iteco','password': 'Iteco@2010'}
 
 net_connect = ConnectHandler(**huawei_5720)
 
-output = net_connect.send_command("disp ip int brief")
+output = net_connect.send_config_from_file('display_commands.txt')
 print(output)
-output = net_connect.send_command("disp device")
-print(output)
+
 net_connect.disconnect()
