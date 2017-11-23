6f30eefa6732521666b59d2d9111f291c3f6be57
Vitaly Nesterov <33909972+nestvit@users.noreply.github.com>
2017-11-23 17:55:28 +0300

    3

--- QueryInventory_S5720.py
+++ QueryInventory_S5720.py
@@ -5,26 +5,20 @@ from datetime import datetime
 import paramiko
 import logging
 import re
-
-huawei_5720 = {}
+import pprint
 
 logging.basicConfig(filename='test.log', level=logging.DEBUG)
 logger = logging.getLogger("netmiko")
-
+huawei_5720 = {}
 try:
-    pass
-    name_module =6
+    huawei_5720 = {'device_type': 'huawei_ssh', 'ip': '172.26.30.78', 'username': 'iteco', 'password': 'Iteco@2010'}
+    net_connect = ConnectHandler(**huawei_5720)
+    output = net_connect.send_config_from_file('display_commands.txt')
+    print(output)
+    net_connect.disconnect()
+    name_module = "Netmiko"
 except ():
     Print ( "Ошибка в модуле: "  + str(name_module))
 finally:
     print("Модуль " + str(name_module) + " - OK")
 
-
-huawei_5720 = {'device_type': 'huawei_ssh','ip': '172.26.30.78','username': 'iteco','password': 'Iteco@2010'}
-
-net_connect = ConnectHandler(**huawei_5720)
-
-output = net_connect.send_config_from_file('display_commands.txt')
-print(output)
-
-net_connect.disconnect()
