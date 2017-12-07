e2de3b78b68891fbca3255c18ebe9cc2a011c6d4
nestvit <33909972+nestvit@users.noreply.github.com>
2017-12-07 23:24:20 +0300

    zxxzx

--- parser.py
+++ parser.py
@@ -1,8 +1,39 @@
 
 import re
 
-f = open("out.txt")
-data  = f.read()
-f.close()
+with open("out.txt") as f:
+    data  = f.read()
 
 print(data)
+
+search = '\<NAK07\S*>'
+match = re.search(search,data)
+print("Sysname: " + match.group())
+
+search = 'HUAWEI [^TECH](\S*)'
+match = re.search(search,data)
+print("Type: " + match.group(1))
+
+search = 'S5720 (\w*)'
+match = re.search(search,data)
+print("Version: " + match.group(1))
+
+search = 'BoardType=(\S*)'
+match = re.findall(search,data)
+print(match)
+
+search = 'BarCode=(\S*)'
+match = re.findall(search,data)
+print(match)
+
+search = 'Item=(\S*)'
+match = re.findall(search,data)
+print(match)
+
+search = 'Manufactured=(\S*)'
+match = re.findall(search,data)
+print(match)
+
+search = 'VendorName=(\S*)'
+match = re.findall(search,data)
+print(match)
