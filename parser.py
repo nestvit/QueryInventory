2282d249d3af91670581dd8e1df75ad2668b6b35
nestvit <33909972+nestvit@users.noreply.github.com>
2017-12-24 17:38:21 +0300

    zxxzx

--- parser.py
+++ parser.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'nestvit'
 
-
 import re
 import os, sys
 import sqlite3
@@ -10,7 +9,7 @@ import pprint
 with open("out.txt") as f:
     data  = f.read()
 
-print(data)
+# print(data)
 
 con = sqlite3.connect('C:/Users/NestVit/PycharmProjects/QueryInventory_S5720/inventory.sqlite')
 cur = con.cursor()
@@ -24,11 +23,9 @@ search = 'HUAWEI [^TECH](\S*)'
 match = re.search(search,data)
 print("Type: " + match.group(1))
 
-
 search = 'S5720 (\w*)'
 match = re.search(search,data)
 print("Version: " + match.group(1))
-
 
 regex = ('BoardType=(\S*)\n'
         'BarCode=(\S*)\n'
@@ -40,19 +37,20 @@ regex = ('BoardType=(\S*)\n'
 result = re.findall(regex,data)
 # print(result)
 
-for row in result:
-    print (row)
-    for item in row:
-        print (item)
+# for row in result:
+#     print (row)
+#     for item in row:
+#         print (item)
 
 for row in result:
-    text = 'INSERT INTO "main"."inventory2" ("Hostname","BoardType","BarCode","Item","Manufactured","VendorName")\
-            VALUES (' + hostname + ',' + str(row[0]) + ',' + str(row[1]) + ',' + str(row[2]) + \
-           ',' + str(row[4]) + ',' + str(row[5]) + ')'
-    print(text)
+    text = 'INSERT INTO "main"."inventory2" ("Hostname","BoardType","BarCode","Item","Description","Manufactured","VendorName")\
+            VALUES ("' + hostname + '","' + str(row[0]) + '","' + str(row[1]) + '","' + str(row[2]) + \
+           '","' + str(row[3])+ '","' + str(row[4]) + '","' + str(row[5]) + '")'
+    # print(text)
     cur.execute(text)
 
 con.commit()
+
 
 
 
