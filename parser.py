00f06c343bc1810a34b1ee96dd627ce4a7283c3c
nestvit <33909972+nestvit@users.noreply.github.com>
2017-12-26 23:43:30 +0300

    Add Table Nodes to SQL database If hostname is exist then update SW, HW etc.

--- parser.py
+++ parser.py
@@ -5,11 +5,15 @@ import re
 import os, sys
 import sqlite3
 import pprint
+import datetime
+
+now = datetime.datetime.now()
+CurrentData = str(now.year) + "-" + str(now.month) + "-" + str(now.day)
+CurrentTime = str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
 
 with open("out.txt") as f:
     data  = f.read()
-
-# print(data)
+#print(data)
 
 con = sqlite3.connect('C:/Users/NestVit/PycharmProjects/QueryInventory_S5720/inventory.sqlite')
 cur = con.cursor()
@@ -20,12 +24,14 @@ print("Sysname: " + match.group(1))
 hostname = match.group(1)
 
 search = 'HUAWEI [^TECH](\S*)'
-match = re.search(search,data)
-print("Type: " + match.group(1))
+Type = re.search(search,data)
+print("Type: " + Type.group(1))
+Type = Type.group(1)
 
 search = 'S5720 (\w*)'
-match = re.search(search,data)
-print("Version: " + match.group(1))
+SW_version = re.search(search,data)
+print("Version: " + SW_version.group(1))
+SW_version = SW_version.group(1)
 
 regex = ('BoardType=(\S*)\n'
         'BarCode=(\S*)\n'
@@ -42,12 +48,27 @@ result = re.findall(regex,data)
 #     for item in row:
 #         print (item)
 
+Segment = "KKS"
+Status = "Normal"
+
 for row in result:
     text = 'INSERT INTO "main"."inventory2" ("Hostname","BoardType","BarCode","Item","Description","Manufactured","VendorName")\
             VALUES ("' + hostname + '","' + str(row[0]) + '","' + str(row[1]) + '","' + str(row[2]) + \
            '","' + str(row[3])+ '","' + str(row[4]) + '","' + str(row[5]) + '")'
     # print(text)
     cur.execute(text)
+
+text = 'INSERT INTO "main"."Nodes" ("segment","Hostname","HW_type","SW_version","Status","Data","Time")\
+            VALUES ("' + str(Segment) + '","' + str(hostname)  + '","' + str(Type) + '","' + str(SW_version) + \
+           '","' + str(Status) + '","' + CurrentData + '","' + CurrentTime + '")'
+try:
+    con.execute(text)
+except sqlite3.IntegrityError as e:
+    text = 'UPDATE nodes set HW_type = "' + str(Type) + '", SW_version = "' + str(SW_version) + '", \
+    Status = "' + str(Status) + '", Data = "' + CurrentData + '", Time = "' + CurrentTime + '" \
+    WHERE Hostname = "' + str(hostname) + '"'
+    con.execute(text)
+    #print("Error occured: ", e)
 
 con.commit()
 
